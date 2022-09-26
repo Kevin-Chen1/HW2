9bcf68f12886089cf9d8f5ab3ff31a7545e22942
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-09-26 14:18:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php require_once("header.php"); ?>
     <h1>Hello, world!</h1>
+
+<?php
+function HelloFunction(){
+    echo "peepee poopoo";
+}
+// Calling function
+HelloFunction();
+?>
+
     <form action="Handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
