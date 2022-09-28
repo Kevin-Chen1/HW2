77cb7ab3b9c1f7375ea2748e1f6b92b4081c0121
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-09-28 13:25:28 -0500

    Create Handleget.php

---
+++ Handleget.php
@@ -0,0 +1,7 @@
+<?php require_once("header.php"); ?>
+
+    <h1>Post response</h1>
+    Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
+    <?php require_once("footer.php"); ?>
